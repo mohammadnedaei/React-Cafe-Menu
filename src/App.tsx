049e9d1aad762ab74750a090104e9529496ff8ef
mohammadnedaei <nedaei2002@gmail.com>
2023-10-14 20:49:44 +0330

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Header from "./assets/components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Header />
+          </Grid>
+          <Grid item xs={4}>
+            <div>2</div>
+          </Grid>
+          <Grid item xs={4}>
+            <div>3</div>
+          </Grid>
+          <Grid item xs={8}>
+            <div>4</div>
+          </Grid>
+        </Grid>
+      </Box>
   )
 }
 
